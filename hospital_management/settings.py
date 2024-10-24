44fa73980496d98626ac247f6f4dc58d31840899
VISHNU C M <149654287+vishnnu-vishnu@users.noreply.github.com>
2024-10-24 08:31:45 +0530

    Update settings.py

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -28,8 +28,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'vishnudemo608@gmail.com'
-EMAIL_HOST_PASSWORD = 'jtdnxvvueperuliq'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 
